75bec9fa1ac2ef57defe2b87025acce3d101c95d
surya474 <gantasuryateja474@gmail.com>
2018-10-31 14:27:52 +0530

    commiit

--- services/Users/bookRideService.js
+++ services/Users/bookRideService.js
@@ -1,27 +1,66 @@
+var express = require('express');
+var app  = express();
+var server = app.listen(8810)
+var io = require('socket.io').listen(server);
 
-var getNearcabsService=require('./getNearCabsService')
+// var http = require('http').Server(app);
+// var io   = require('socket.io')(http);
 
-async function confirmRide(reqData){          
- console.log("in req data",reqData)  
- return new Promise(resolve=>{
-    getNearcabsService.getNearCabs(reqData,function(res){
-        console.log("in confirm ride response",res)
-        var cabsData=res.Data
-        cabsData.sort(function(a, b){   
-                return a.distanceValue-b.distanceValue
-            })
-     resolve(res)
-    },(err)=>{       
-  resolve(err)   
-    })
- })
-  
+
+var getNearcabsService = require('./getNearCabsService')
+
+async function confirmRide(reqData) {
+  console.log("in req data", reqData)
+  return new Promise(resolve => {
+    getNearcabsService.getNearCabs(reqData, function (res) {
+      var tripID= + new Date()
+    console.log(tripID)
+      console.log("in confirm ride response", res)
+      var cabsData = res.Data
+      cabsData.sort(function (a, b) {
+        return a.distanceValue - b.distanceValue
+      })
          
-}        
-                           
+let cabsarr=
+resolve(cabsData)
+sendMessagetoCabs(reqData,cabsData,tripID)
+
+
+    }, (err) => {
+      resolve(err)
+    })
+  })
+}
+
+
+async function sendMessagetoCabs(reqData,cabsData,tripID){
+
+
+  for(let i=0;i<cabsData.length;i++){
+    io.on('connection', function(socket){
+      
+        socket.on('driverResp',(data)=>{
+            
+        })
+        });
+
+let sendData=await emitData(reqData,cabsData[0],tripID)
+
+  }
+  
+
+}
+
+async function emitData(reqData,cabData,tripId){
+  io.on('connection', function(socket){
+let obj=reqData
+    obj["driverId"]=cabData._id
+    obj["tripId"]=tripId
+    socket.emit('reqCab', obj);
+  });
+}
 
 module.exports = ({
-    confirmRide: confirmRide, 
-  
-  })
-    
+  confirmRide: confirmRide,
+
+})
